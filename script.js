413fcb8a959bb68da98c6de795e2323d1d6869d6
NejiH <hayon.arnaud@gmail.com>
2024-07-18 11:11:09 +0200

    update correspondances genres caegories

--- script.js
+++ script.js
@@ -12,40 +12,42 @@ let button = document.getElementById('button');
 let image = document.getElementById('image');
 let filmId = document.getElementById('filmid');
 let filmTitle = document.getElementById('title');
-let moodObject = {"happy": [28,16,35,10751,14],
-                  "sad" : [28,16,35,10751,14],
-                  "comfy" : [28,16,35,10751,14],
-                  "action" : [28,16,35,10751,14],
-                  "detective" : [28,16,35,10751,14],
-                  "adult" : [28,16,35,10751,14]
-                }
+let moodObject = {
+  "happy": [80, 99, 18, 36, 10770, 53, 10752, 37],
+  "sad": [28, 80, 99, 10751, 14, 36, 27, 10402, 9648, 10770, 53, 10752, 37],
+  "goofy": [80, 99, 18, 36, 10402, 9648, 10749, 878, 10770, 53, 10752, 37], // prevenir changement de nom
+  "in-love": [28, 80, 99, 18, 10751, 36, 27, 9648, 10770, 53, 10752, 37],
+  "nerdy": [16, 35, 18, 10751, 27, 10402, 10749, 10770, 10752, 37],
+  "angry": [16, 14, 36, 27, 10402, 9648, 10749, 878, 10770],
+  "surpise-me": [10770]
+}
 
 
 const changeFilm = async () => {
-    //let randomNumber = Math.ceil(Math.random() * 1000) + 1;
-    //let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-    let mod = window.location.search.replace("?", "");
-    //console.log(mod);
-    
-    let id_film =   await getRandomFilm();
-    let requestString = `https://api.themoviedb.org/3/movie/${id_film}?language=en-US`;
+  //let randomNumber = Math.ceil(Math.random() * 1000) + 1;
+  //let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+  let mod = window.location.search.replace("?", "");
+  //console.log(mod);
 
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OWJhZTE2YjkwZWMxYzk0ZDYwZWY4YTE1M2NiODY2MyIsIm5iZiI6MTcyMTA1MTk4OS4yMTkyMzYsInN1YiI6IjY2OTUyYTVmZTkyYWI0ZDMyMGJhY2FkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-nZFMjmWuNpcTBkHSgnKCrePtpiRctjIFaUQ5nIR9XY'
-        }
-      };
+  let id_film = await getRandomFilm();
+  let requestString = `https://api.themoviedb.org/3/movie/${id_film}?language=en-US`;
 
-    let data = await fetch(requestString, options);
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OWJhZTE2YjkwZWMxYzk0ZDYwZWY4YTE1M2NiODY2MyIsIm5iZiI6MTcyMTA1MTk4OS4yMTkyMzYsInN1YiI6IjY2OTUyYTVmZTkyYWI0ZDMyMGJhY2FkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-nZFMjmWuNpcTBkHSgnKCrePtpiRctjIFaUQ5nIR9XY'
+    }
+  };
 
-    let response = await data.json();
-    console.log(response);
+  let data = await fetch(requestString, options);
 
-    image.src = `https://image.tmdb.org/t/p/w500/${response.poster_path}`;
-    filmId.textContent = `#${response.id}`;
-    filmTitle.textContent = response.original_title;
+  let response = await data.json();
+  console.log(response);
+
+  image.src = `https://image.tmdb.org/t/p/w500/${response.poster_path}`;
+  filmId.textContent = `#${response.id}`;
+  filmTitle.textContent = response.original_title;
 }
 
 const FilmDataListByMood = async (pagenbr) => {
@@ -57,25 +59,24 @@ const FilmDataListByMood = async (pagenbr) => {
   let without_genres = ''
   /* tabe_genres = [28,16,35,10751,14]
   with_genres = `&with_genres=${tabe_genres.join("%2C")}` */
-  let requestString ='https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+  let requestString = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
   console.log(mood in moodObject);
-  if (mood in moodObject)
-  {
+  if (mood in moodObject) {
     tabe_genres = moodObject[mood]
     with_genres = `&with_genres=${tabe_genres.join("%2C")}`
     requestString = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pagenbr}&sort_by=popularity.desc${with_genres}${without_genres}`;
   }
   //without_genres = `&without_genres=${tabe_genres.join("%2C")}`
-  
+
   //let requestString = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pagenbr}&sort_by=popularity.desc${with_genres}${without_genres}`;
   console.log(requestString);
   const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OWJhZTE2YjkwZWMxYzk0ZDYwZWY4YTE1M2NiODY2MyIsIm5iZiI6MTcyMTA1MTk4OS4yMTkyMzYsInN1YiI6IjY2OTUyYTVmZTkyYWI0ZDMyMGJhY2FkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-nZFMjmWuNpcTBkHSgnKCrePtpiRctjIFaUQ5nIR9XY'
-      }
-    };
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OWJhZTE2YjkwZWMxYzk0ZDYwZWY4YTE1M2NiODY2MyIsIm5iZiI6MTcyMTA1MTk4OS4yMTkyMzYsInN1YiI6IjY2OTUyYTVmZTkyYWI0ZDMyMGJhY2FkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-nZFMjmWuNpcTBkHSgnKCrePtpiRctjIFaUQ5nIR9XY'
+    }
+  };
 
   let data = await fetch(requestString, options);
   let response = await data.json();
@@ -86,10 +87,10 @@ const getRandomFilm = async () => {
   let responseNbPage = await FilmDataListByMood(1);
   console.log(responseNbPage);
   console.log(responseNbPage.total_pages);
-  randnbr = Math.floor(Math.random() * responseNbPage.total_pages) + 1; 
+  randnbr = Math.floor(Math.random() * responseNbPage.total_pages) + 1;
   /* console.log(randnbr); */
   let responseListPage = await FilmDataListByMood(randnbr);
-  let randnbrFilm = Math.floor(Math.random() * responseListPage.results.length); 
+  let randnbrFilm = Math.floor(Math.random() * responseListPage.results.length);
   console.log(randnbrFilm);
   console.log(responseListPage.results[randnbrFilm]);
   console.log(responseListPage.results[randnbrFilm].id);
