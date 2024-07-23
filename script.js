9634731c6320d0e0f46ff12d0b944ad6bc3f94b4
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-07-23 11:53:53 +0200

    ajout backdrop path and movie details

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// const url = 'https://api.adviceslip.com/advice';
-
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//         let adviceElement = document.createElement('p');
-//         adviceElement.innerHTML = `Conseil du jour : ${data.slip.advice}`;
-//         document.body.appendChild(adviceElement);
-//     });
-
 let button = document.getElementById('button');
 let image = document.getElementById('image');
 let filmId = document.getElementById('filmid');
@@ -16,6 +6,7 @@ let buttonLink = document.getElementById('button-link');
 let filmYear = document.getElementById('year');
 let filmDuration = document.getElementById('duration');
 let filmReleaseDate = document.getElementById('release-date');
+let averageNote = document.getElementById('average-note');
 let filmSynopsis = document.getElementById('synopsis');
 let filmGenre = document.getElementById('genre');
 let backgroundImage = document.getElementById('background-image');
@@ -23,7 +14,7 @@ let tableauGenre = [];
 let affichageGenre = ''
 
 let moodObject = {
-  "happy": [80, 99, 18, 36, 10770, 53, 10752, 37],
+  "happy": [80, 99, 18, 36, 27, 10770, 53, 10752, 37],
   "sad": [28, 80, 99, 10751, 14, 36, 27, 10402, 9648, 10770, 53, 10752, 37],
   "goofy": [28, 80, 99, 18, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37],
   "in-love": [28, 80, 99, 18, 10751, 36, 27, 9648, 10770, 53, 10752, 37],
@@ -31,6 +22,8 @@ let moodObject = {
   "angry": [16, 14, 36, 27, 10402, 9648, 10749, 878, 10770],
   "surpise-me": [10770]
 }
+
+// Fonction principale 
 
 const changeFilm = async () => {
 
@@ -57,18 +50,10 @@ const changeFilm = async () => {
   filmTitle.innerHTML = `<b>${response.title}</b> (${response.release_date.substr(0, 4)})`;
   filmDuration.innerHTML = `<b>Durée</b> : ${response.runtime} min`;
   filmReleaseDate.innerHTML = `<b>Date de sortie</b> : ${response.release_date}`;
+  averageNote.innerHTML = `<b>Note :</b> ${response.vote_average.toFixed(1)}/10`;
   filmSynopsis.innerHTML = `${response.overview}`;
 
   buttonLink.href = `https://www.themoviedb.org/movie/${response.id}/watch`;
-
-  // for (let i = 0; i < response.genres.length; i++) {
-  //   tableauGenre.push(response.genres[i].name)
-  // }
-
-  // for (let i = 0; i < tableauGenre.length; i++) {
-  //   affichageGenre += `<p>${tableauGenre[i]}</p>`
-  // }
-  // filmGenre.innerHTML = affichageGenre
 
   response.genres.forEach(genre => {
     let genreElement = document.createElement('p');
@@ -77,20 +62,15 @@ const changeFilm = async () => {
     filmGenre.appendChild(genreElement);
   })
 
-
-
-
 }
 
+// Filtre les résultats par genre en fonction du mood
+
 const FilmDataListByMood = async (pagenbr) => {
-  //let randomNumber = Math.ceil(Math.random() * 1000) + 1;
-  //let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
   let mood = window.location.search.replace("?", "");
   console.log(mood);
   let with_genres = ''
   let without_genres = ''
-  /* tabe_genres = [28,16,35,10751,14]
-  with_genres = `&with_genres=${tabe_genres.join("%2C")}` */
   let requestString = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&vote_average.gte=5&vote_count.gte=200&watch_region=FR&with_watch_monetization_types=flatrate%7Cfree%7Cads%7Crent%7Cbuy';
   console.log(mood in moodObject);
   if (mood in moodObject) {
@@ -99,9 +79,7 @@ const FilmDataListByMood = async (pagenbr) => {
     without_genres = `&without_genres=${tabe_genres.join("%2C")}`
     requestString = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pagenbr}&sort_by=popularity.desc&vote_average.gte=5&vote_count.gte=200&watch_region=FR&with_watch_monetization_types=flatrate%7Cfree%7Cads%7Crent%7Cbuy${with_genres}${without_genres}`;
   }
-  //without_genres = `&without_genres=${tabe_genres.join("%2C")}`
 
-  //let requestString = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pagenbr}&sort_by=popularity.desc${with_genres}${without_genres}`;
   console.log(requestString);
   const options = {
     method: 'GET',
@@ -117,28 +95,26 @@ const FilmDataListByMood = async (pagenbr) => {
   return (response);
 }
 
+
+// Fonction random sur le nombre de pages de résultats, limité à 500 pages
+
 const getRandomFilm = async () => {
   let randnbr = 101;
   let responseNbPage = await FilmDataListByMood(1);
-  console.log(responseNbPage);
-  console.log(responseNbPage.total_pages);
+ 
   if (responseNbPage.total_pages > 500) {
     randnbr = Math.floor(Math.random() * 499) + 1;
   }
   else {
     randnbr = Math.floor(Math.random() * responseNbPage.total_pages) + 1;
   }
-  /* console.log(randnbr); */
   let responseListPage = await FilmDataListByMood(randnbr);
   let randnbrFilm = Math.floor(Math.random() * responseListPage.results.length);
-  console.log(randnbrFilm);
   console.log(responseListPage.results[randnbrFilm]);
-  //console.log(responseListPage.results[randnbrFilm].id);
   return (responseListPage.results[randnbrFilm].id);
 }
 
 changeFilm();
 
 button.addEventListener('click', changeFilm);
-//getRandomFilm();
 
