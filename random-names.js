8d962edea7054fc29ad4cf5e6bae397db53807bc
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-07-24 16:49:24 +0200

    final homepage

--- random-names.js
+++ random-names.js
@@ -1,4 +1,4 @@
-const randomName = ['Beautiful', 'Master', 'Super Loutre', 'Sunshine', 'Nono', 'Cuty Pie', 'Super Castor', 'Butter Cup', 'Handsome', 'Muffin', 'Pumpkin', 'Cupcake', 'Little Schtroumpf', 'Little One', 'to the Stongest', 'Petite Chose Mignonne', 'You']
+const randomName = ['Beautiful', 'Master', 'Super Loutre', 'Sunshine', 'Nono', 'Cuty Pie', 'Super Castor', 'Butter Cup', 'Handsome', 'Muffin', 'Pumpkin', 'Kwazy Cupcake', 'Little Schtroumpf', 'Little One', 'to the Stongest', 'Petite Chose Mignonne', 'You']
 
 function getRandomName() {
     return randomName[Math.floor(Math.random() * randomName.length)];
@@ -6,7 +6,7 @@ function getRandomName() {
 }
 
 function displayRandomName() {
-    document.getElementById('randomname').innerHTML = `Hello <span><em>${getRandomName()}</em></span>!`;
+    document.getElementById('randomname').innerHTML = `Hello <span><em>${getRandomName()}</em></span> !`;
 }
 
 displayRandomName()
