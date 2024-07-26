5142e83fa236cba6c5a67b9c137346f860a5bcb3
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-07-26 10:36:44 +0200

    personal project - first commit

--- random-names.js
+++ random-names.js
@@ -1,6 +1,7 @@
 // Random name sur la homepage
 
-const randomName = ['Beautiful', 'Master', 'Super Loutre', 'Sunshine', 'Nono', 'Cuty Pie', 'Super Castor', 'Butter Cup', 'Handsome', 'Muffin', 'Pumpkin', 'Kwazy Cupcake', 'Little Schtroumpf', 'Little One', 'to the Stongest', 'Petite Chose Mignonne', 'You']
+// const randomName = ['Beautiful', 'Super Loutre', 'Sunshine', 'Nono', 'Cuty Pie', 'Super Castor', 'Butter Cup', 'Handsome', 'Muffin', 'Pumpkin', 'Kwazy Cupcake', 'Little Schtroumpf', 'Little One', 'Master', 'Petite Chose Mignonne', 'You']
+const randomName = ['Super Loutre', 'Cuty Pie', 'Super Castor', 'Butter Cup', 'Muffin', 'Pumpkin', 'Kwazy Cupcake', 'Little Schtroumpf','Petite Chose Mignonne']
 
 function getRandomName() {
     return randomName[Math.floor(Math.random() * randomName.length)];
@@ -8,7 +9,7 @@ function getRandomName() {
 }
 
 function displayRandomName() {
-    document.getElementById('randomname').innerHTML = `Hello <span><em>${getRandomName()}</em></span> !`;
+    document.getElementById('randomname').innerHTML = `Bonjour <span><em>${getRandomName()}</em></span> !`;
 }
 
 displayRandomName()
